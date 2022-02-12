02ff25a0727b4845d296044dc76a99d1d40c13d7
BakayAlexander <bakay.dvr@gmail.com>
2022-02-12 09:11:42 +0300

    fix: gh pages

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,12 @@ import Blog from './Blog';
 import Contact from './Contact';
 import NotFound from './NotFound';
 import { BrowserRouter, Redirect } from 'react-router-dom';
-import NavBar from './NavBar';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="page">
         <Switch>
-          <Route path="/blog-react" component={() => <Redirect to="/" />}></Route>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/blog" component={Blog} />
