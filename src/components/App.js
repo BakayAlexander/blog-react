344b8e92eab7fe1b2869b83e9c2d071b8e0b7c1b
BakayAlexander <bakay.dvr@gmail.com>
2022-05-31 16:58:50 +0300

    fix for Netlify

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,22 @@ import Home from './Home';
 import Blog from './Blog';
 import Contact from './Contact';
 import NotFound from './NotFound';
-import { BrowserRouter, Redirect } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="page">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/blog" component={Blog} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<div className='page'>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/about' component={About} />
+					<Route exact path='/blog' component={Blog} />
+					<Route exact path='/contact' component={Contact} />
+					<Route exact path='*' component={NotFound} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
