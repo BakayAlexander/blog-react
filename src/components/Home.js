aa83d18431cd9819e2b7733725cfa910a2490523
BakayAlexander <bakay.dvr@gmail.com>
2022-05-31 16:52:45 +0300

    fix for netlify

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,18 @@ import NavBar from './NavBar';
 import TypeWriter from './TypeWriter';
 
 function Home() {
-  return (
-    <div className="home">
-      <NavBar />
-      <h1
-        className="home__title
-  "
-      >
-        Welcome
-      </h1>
-      <TypeWriter>Test blog by Alexander Bakay</TypeWriter>
-    </div>
-  );
+	return (
+		<div className='home'>
+			<NavBar />
+			<h1
+				className='home__title
+  '
+			>
+				Welcome
+			</h1>
+			<TypeWriter>Test project blog by Alexander Bakay</TypeWriter>
+		</div>
+	);
 }
 
 export default React.memo(Home);
